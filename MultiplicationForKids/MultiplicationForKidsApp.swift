f638e60185defe45114d9081565c3ebbd5f0f7a2
stevegustason <steve.gustason@gmail.com>
2023-04-04 13:59:36 -0500

    Initial Commit

---
+++ MultiplicationForKids/MultiplicationForKidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationForKidsApp.swift
+//  MultiplicationForKids
+//
+//  Created by Steven Gustason on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationForKidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
